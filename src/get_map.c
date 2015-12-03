41d8d60e1cdff87bcf16dae50b2b5357425c6248
Raphael BAUM <rbaum@e1r10p13.42.fr>
2015-12-03 20:41:24 +0100

    regular commit

--- src/get_map.c
+++ src/get_map.c
@@ -12,33 +12,44 @@
 
 #include "fillit.h"
 
-void			check(char *line)
+void			check(char *line, int j)
 {
 	int			i;
+	static int	k = 0;
 
 	i = 0;
 	if (ft_strlen(line) != 4 && line[0] != '\0')
-		ft_exit("Bad bad not good");
+		ft_exit("Bad bad not good\n");
 	while (line[i])
 	{
+		if (line[i] == '#')
+			k++;
+			j = j % 5;
 		if (line[i] != '.' && line[i] != '\0' && line[i] != '#')
-			ft_exit("Bad char, please only use . and #");
+			ft_exit("Bad char, please only use . and #\n");
 		i++;
 	}
-		ft_putendl(line);
+	if (j == 4 && k != 4)
+		ft_exit("Invalid piece\n");
+	else if (j == 4)
+		k = 0;
+	ft_putendl(line);
 }
 
 int				verif(char *name)
 {
 	int			fd;
+	int			j;
 	char		*line;
 
 	fd = open (name, O_RDONLY);
+	j = 1;
 	if (fd < 0)
 		return (0);
 	while (get_next_line(fd, &line))
 	{
-		check(line);
+		check(line, j);
+		j++;
 	}
 	return (1);
 }
